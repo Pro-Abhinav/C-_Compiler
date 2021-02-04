5155e69f19999ded159cb5799feafbd1fdbd50e0
Pro-Abhinav <abhinav16032002@gmail.com>
2021-02-04 23:48:10 +0530

    added evaluation result code

---
+++ Compiler/CodeAnalysis/Syntax/AssignmentExpressionSyntax.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+
+namespace Compiler.CodeAnalysis.Syntax
+{
+    public sealed class AssignmentExpressionSyntax : ExpressionSyntax
+    {
+        public AssignmentExpressionSyntax(SyntaxToken identifierToken, SyntaxToken equalsToken, ExpressionSyntax expression)
+        {
+            IdentifierToken = identifierToken;
+            EqualsToken = equalsToken;
+            Expression = expression;
+        }
+         
+        public override SyntaxKind Kind => SyntaxKind.AssignmentExpression;
+        
+        public SyntaxToken IdentifierToken { get; }
+        public SyntaxToken EqualsToken { get; }
+        public ExpressionSyntax Expression { get; }
+    }
+}
