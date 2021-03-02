0c014d8f0b6c52b82bf35166e42957971bf86ba9
Pro-Abhinav <abhinav16032002@gmail.com>
2021-03-03 01:51:55 +0530

    modified file

---
+++ Compiler/CodeAnalysis/Syntax/NameExpressionSyntax.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+
+namespace Compiler.CodeAnalysis.Syntax
+{
+    public sealed class NameExpressionSyntax : ExpressionSyntax
+    {
+        public NameExpressionSyntax(SyntaxToken identifierToken )
+        {
+            IdentifierToken = identifierToken;
+        }
+
+        public SyntaxToken IdentifierToken { get; }
+
+        public override SyntaxKind Kind => SyntaxKind.NameExpression;
+    }
+}
